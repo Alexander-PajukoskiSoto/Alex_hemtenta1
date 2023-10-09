30ab3c095b483f68ff9de1b7f4af067095de6d48
Alexander Pajukoski Soto <alexander.pajukoskisoto@elev.ga.ntig.se>
2023-10-09 15:37:06 +0200

    array local storage :)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
+
+let parsedItems = JSON.parse(localStorage.getItem("isTask"))
+let items = parsedItems;
+if(localStorage.getItem("isTask")===undefined || localStorage.getItem("isTask")=== null){
+    items=[];
+}
+
 const addTask = event => { 
     event.preventDefault();
-
-    var newTask = document.getElementById("toDoTask").value;
-    localStorage.setItem("isTask", newTask);
+    let newTask = document.getElementById("toDoTask").value;
+    items.push(newTask);
+    localStorage.setItem("isTask", JSON.stringify(items));
 }
