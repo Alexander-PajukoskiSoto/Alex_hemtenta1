e46377cbadd50c0904c9b48d08522a78eb2068b3
Alexander Pajukoski Soto <alexander.pajukoskisoto@elev.ga.ntig.se>
2023-10-09 15:58:54 +0200

    css pretty good adde functiopn also makes html when called just need append latera

--- js/script.js
+++ js/script.js
@@ -5,6 +5,16 @@ if(localStorage.getItem("isTask")===undefined || localStorage.getItem("isTask")=
     items=[];
 }
 
+const generateHTML = html=>{
+    const template = document.createElement("template")
+    template.innerHTML = html.trim();
+    return template.content.firstElementChild;
+}
+
+const newTaskElemnt = generateHTML('<div class="input-group mb-3"><div class="input-group-prepend"><div class="input-group-text"><input type="checkbox" aria-label="Checkbox for following text input"></div></div><input type="text" class="form-control" aria-label="Text input with checkbox"></div>');
+
+console.log(newTaskElemnt);
+
 const addTask = event => { 
     event.preventDefault();
     let newTask = document.getElementById("toDoTask").value;
