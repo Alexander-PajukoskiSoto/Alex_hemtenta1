34c8289b05dc23b1d26383bd9021718fac7957bd
Alexander Pajukoski Soto <alexander.pajukoskisoto@elev.ga.ntig.se>
2023-10-09 14:18:21 +0200

    Addded forms abnd stuyfff not working tho ????!

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
-document.getElementById("clickableDiv").addEventListener(
-    "mouseover", ()=> myFunction()
-);
+const addTask = event => { 
+    event.preventDefault();
+
+    var newTask = document.getElementById("toDoTask").value;
+    localStorage.setItem("isTask", newTask);
+}
